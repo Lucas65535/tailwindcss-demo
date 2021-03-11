409d7cc0db0c7945e8547992be1caf792a337471
youkoucoding <youkousai@gmail.com>
2021-03-11 21:56:17 +0900

    demo6 && add custom colors

--- demo6-landingpage_nextjs/pages/index.js
+++ demo6-landingpage_nextjs/pages/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 export default function Index() {
   return (
-    <div>
-      <h1 className="text-gray-500 text-3xl text-center">Hello Tailwindcss</h1>
+    <div className="">
+      <h1 className="text-gray-500 text-3xl">Tailwindcss GROWEX</h1>
+      <p className="text-3xl">Logo</p>
+
+      <h1>
+        want to <span className="text-primary">speed less time</span> on social medias
+      </h1>
+
+      <h2>
+        Amazing things will happen when you connect your social media accounts with GROWEX
+      </h2>
+
+      <input type="email" placeholder="ex: gmail@gmail.com" />
+      <button>Get Start for free</button>
+
     </div>
   );
 }
--- demo6-landingpage_nextjs/tailwind.config.js
+++ demo6-landingpage_nextjs/tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4960F9",
+        secondary: "#F8F8F8",
+      },
+    },
   },
   variants: {
     extend: {},
