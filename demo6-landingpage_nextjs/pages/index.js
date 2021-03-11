629b6228eff6e37b88f623a9708698a46106ef3a
youkoucoding <youkousai@gmail.com>
2021-03-11 21:27:19 +0900

    start demo6 with nextjs & tailwindcss

---
+++ demo6-landingpage_nextjs/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Index() {
+  return (
+    <div>
+      <h1 className="text-gray-500 text-3xl text-center">Hello Tailwindcss</h1>
+    </div>
+  );
+}
