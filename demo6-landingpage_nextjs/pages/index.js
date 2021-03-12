ef82dd24691e7154ca5d7fd39cb7bd31b7031985
youkoucoding <youkousai@gmail.com>
2021-03-12 21:50:33 +0900

    demo6 && add theme

--- demo6-landingpage_nextjs/pages/index.js
+++ demo6-landingpage_nextjs/pages/index.js
@@ -2,21 +2,24 @@ import React from 'react';
 
 export default function Index() {
   return (
-    <div className="">
-      <h1 className="text-gray-500 text-3xl">Tailwindcss GROWEX</h1>
-      <p className="text-3xl">Logo</p>
+    <div>
+      <div className="flex justify-between">
+        <h1 className="text-lg">Tailwindcss GROWEX</h1>
+        <p>Logo</p>
+      </div>
 
-      <h1>
-        want to <span className="text-primary">speed less time</span> on social medias
-      </h1>
+      <div className="px-4">
+        <h3 className="text-h3 text-center text-grayPrimary tracking-wide">
+          want to <span className="text-primary">speed less time</span> on social medias
+      </h3>
 
-      <h2>
-        Amazing things will happen when you connect your social media accounts with GROWEX
+        <h2 className="text-grayTertiary text-center tracking-wider leading-6">
+          Amazing things will happen when you connect your social media accounts with GROWEX
       </h2>
 
-      <input type="email" placeholder="ex: gmail@gmail.com" />
-      <button>Get Start for free</button>
-
+        <input type="email" placeholder="ex: gmail@gmail.com" />
+        <button>Get Start for free</button>
+      </div>
     </div>
   );
 }
