c919f18517ae3006beaccce3f50ae8bc59e4d993
youkoucoding <youkousai@gmail.com>
2021-03-15 22:41:11 +0900

    demo9 && add hero&googleFont

---
+++ demo9-navbar-page/src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hero() {
+    return (
+        <div className='bg-white h-screen flex flex-col items-center justify-center'>
+            <h1 className='lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-mont font-bold mb-14 text-yellow-500'>excellent</h1>
+        </div>
+    );
+}
+
+export default Hero;
--- demo9-navbar-page/src/components/Navbar.js
+++ demo9-navbar-page/src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className='flex justify-between items-center h-16 font-mono shadow-sm relative text-black bg-white' role='navigation'>
+        <nav className='flex justify-between items-center h-16 font-mont shadow-sm relative text-black bg-white' role='navigation'>
             <Link to="/" className='pl-8'>Navbar</Link>
             <div className='px-4 cursor-pointer md:hidden'>
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
