c0aabd8e861ed656dc306e1f879e14660dc9a116
youkoucoding <youkousai@gmail.com>
2021-03-16 21:35:41 +0900

    demo9 && add content

--- demo9-navbar-page/src/App.js
+++ demo9-navbar-page/src/App.js
@@ -1,12 +1,13 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-
+import Content from './components/Content';
 
 export default function App() {
     return (
         <>
             <Navbar />
             <Hero />
+            <Content />
         </>
     );
 }
---
+++ demo9-navbar-page/src/components/Content.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Content = () => {
+    return (
+        <>
+            <div className='flex flex-col  justify-center items-center h-screen bg-gray-200 font-mont py-24'>
+                <img className='rounded h-full mb-12 shadow' src='https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1287&q=80' alt="pic" />
+                <div className='flex flex-col items-center justify-center'>
+                    <h2 className='text-2xl mb-2'>Egg Muffles</h2>
+                    <p className='mb-2'>Delicious and nutritious.</p>
+                    <span>$16</span>
+                </div>
+            </div>
+
+            <div className='flex flex-col  justify-center items-center h-screen bg-gray-200 font-mont py-24'>
+                <img className='rounded h-full mb-12 shadow' src='https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1287&q=80' alt="pic" />
+                <div className='flex flex-col items-center justify-center'>
+                    <h2 className='text-2xl mb-2'>Egg Muffles</h2>
+                    <p className='mb-2'>Delicious and nutritious.</p>
+                    <span>$16</span>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Content;
