9e51155eae7b9491b321258583ac90b63bbdf0d1
youkoucoding <youkousai@gmail.com>
2021-03-17 15:17:34 +0900

    demo9 && add pages

--- demo9-navbar-page/src/App.js
+++ demo9-navbar-page/src/App.js
@@ -1,14 +1,19 @@
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Content from './components/Content';
 import Footer from './components/Footer';
+import Home from './pages';
+import About from './pages/about';
+import Menu from './pages/menu';
+import { Switch, Route } from 'react-router-dom';
 
 export default function App() {
     return (
         <>
             <Navbar />
-            <Hero />
-            <Content />
+            <Switch>
+                <Route path='/' exact component={Home}></Route>
+                <Route path='/about' exact component={About}></Route>
+                <Route path='/Menu' exact component={Menu}></Route>
+            </Switch>
             <Footer />
         </>
     );
---
+++ demo9-navbar-page/src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function about() {
+    return (
+        <div className='h-screen flex items-center justify-center bg-yellow-300'>
+            <p className='text-7xl font-gray-900 uppercase font-mont font-extrabold'>about page</p>
+        </div>
+    );
+}
+
+export default about;
