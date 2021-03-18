91c78da26ef9a0629a8c907f4c7812b34dc33295
youkoucoding <youkousai@gmail.com>
2021-03-18 21:46:15 +0900

    demo9 && add dropdown and finished

--- demo9-navbar-page/src/App.js
+++ demo9-navbar-page/src/App.js
@@ -1,20 +1,45 @@
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages';
 import About from './pages/about';
 import Menu from './pages/menu';
 import { Switch, Route } from 'react-router-dom';
+import Dropdown from './components/Dropdown';
 
 export default function App() {
-    return (
-        <>
-            <Navbar />
-            <Switch>
-                <Route path='/' exact component={Home}></Route>
-                <Route path='/about' exact component={About}></Route>
-                <Route path='/Menu' exact component={Menu}></Route>
-            </Switch>
-            <Footer />
-        </>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const hiddenMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+        console.log('resized');
+      }
+    };
+
+    window.addEventListener('resize', hiddenMenu);
+
+    return () => {
+      window.removeEventListener('resize', hiddenMenu);
+    };
+
+  });
+
+  return (
+    <>
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <Switch>
+        <Route path='/' exact component={Home}></Route>
+        <Route path='/about' exact component={About}></Route>
+        <Route path='/Menu' exact component={Menu}></Route>
+      </Switch>
+      <Footer />
+    </>
+  );
 }
--- demo9-navbar-page/src/pages/menu.js
+++ demo9-navbar-page/src/pages/menu.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function Menu() {
-    return (
-        <div className='h-screen flex items-center justify-center bg-blue-300'>
-            <p className='text-7xl font-gray-900 uppercase font-mont font-extrabold'>menu page</p>
-        </div>
-    );
+  return (
+    <div className='h-screen flex items-center justify-center bg-blue-300'>
+      <p className='text-7xl font-gray-900 uppercase font-mont font-extrabold'>menu page</p>
+    </div>
+  );
 }
 
 export default Menu;
